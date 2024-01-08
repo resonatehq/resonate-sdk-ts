cfbf17f51c11e271d6892851b86e4377a1001fe4
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-01-08 11:46:52 +0530

    fix lint errors

--- test/browser.test.ts
+++ test/browser.test.ts
@@ -1,5 +1,5 @@
 import { jest, describe, beforeAll, afterAll, test } from "@jest/globals";
-import { Browser, launch, Page } from 'puppeteer';
+import { Browser, launch, Page } from "puppeteer";
 
 let browser: Browser;
 let page: Page;
@@ -10,13 +10,13 @@ describe("Resonate Server Tests", () => {
   beforeAll(async () => {
     browser = await launch();
     // Open a new page
-    [page] = await browser.pages();  
-    await page.goto('https://google.com');
+    [page] = await browser.pages();
+    await page.goto("https://google.com");
   });
-  
+
   afterAll(async () => {
     await browser.close();
   });
 
-  require('./store.test.ts');
+  require("./store.test.ts");
 });
