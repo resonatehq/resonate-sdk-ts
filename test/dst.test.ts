72d00b4c99127e5962014de91d359023644d6faa
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-01-31 15:05:37 +0530

    baseline and test with failures

--- test/dst.test.ts
+++ test/dst.test.ts
@@ -18,20 +18,30 @@ function traces(ctx: Context): TraceTree {
   };
 }
 
+const baseline = new Resonate();
+
+baseline.register("test", test);
+
 const resonate = new Resonate();
 
-resonate.register("test", test);
-resonate.register("test2", test2);
+resonate.register("test", test2);
 
 async function test(ctx: Context) {
+  //   if (Math.random() < 0.1) {
+  //     throw new Error("Simulated failure in foo");
+  //   }
+
   await ctx.run(foo);
   await ctx.run(foo);
 }
 
 async function test2(ctx: Context) {
-    await ctx.run(foo2);
-    await ctx.run(foo2);
-    }
+  if (Math.random() < 0.7) {
+    throw new Error("Simulated failure in foo");
+  }
+  await ctx.run(foo2);
+  await ctx.run(foo2);
+}
 
 async function foo(ctx: Context) {
   await ctx.run(bar);
@@ -39,52 +49,62 @@ async function foo(ctx: Context) {
 }
 
 async function foo2(ctx: Context) {
-    await ctx.run(bar);
+  if (Math.random() < 0.6) {
+    throw new Error("Simulated failure in foo");
   }
+  await ctx.run(bar2);
+  await ctx.run(bar2);
+}
 
 async function bar(ctx: Context) {}
 
+async function bar2(ctx: Context) {
+  if (Math.random() < 0.8) {
+    throw new Error("Simulated failure in foo");
+  }
+}
+
 function isSubsetTree(context: Context, other: Context): boolean {
+  if (context.id !== other.id) {
+    return false;
+  }
 
-    if (context.id !== other.id) {
-        return false;
+  for (const child of context.children) {
+    if (isCompletedPromise(child)) {
+      continue;
     }
 
-    for (const child of context.children) {
-        if (isCompletedPromise(child)) {
-            continue;
-        }
-        
-        const matchingChild = other.children.find((otherchild) => otherchild.id === child.id && otherchild.parent?.id === child.parent?.id);
+    const matchingChild = other.children.find(
+      (otherchild) => otherchild.id === child.id && otherchild.parent?.id === child.parent?.id,
+    );
 
-        if (!matchingChild) {
-            return false; 
-        }
-
-        if (!isSubsetTree(child, matchingChild)) {
-            return false;
-        }
+    if (!matchingChild) {
+      return false;
     }
-    return true;
-}  
+
+    if (!isSubsetTree(child, matchingChild)) {
+      return false;
+    }
+  }
+  return true;
+}
 
 async function main() {
-  const { context, promise } = resonate._run("test", "baseline");
+  const { context, promise } = baseline._run("test", "baseline");
   await promise;
 
   // map context to something useable
   const traceTree = traces(context);
 
   printTree(traceTree);
-  
-  const { context: context2, promise: promise2 } = resonate._run("test2", "baseline");
+
+  const { context: context2, promise: promise2 } = resonate._run("test", "baseline");
   await promise2;
 
   const traceTree2 = traces(context2);
   printTree(traceTree2);
 
   console.log(isSubsetTree(context, context2));
-
 }
 
 function printTree(tree: TraceTree, indent: string = ""): void {
