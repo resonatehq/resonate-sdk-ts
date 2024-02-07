6cf03bd9b28cce52c2506568209f3bc640ec1ee9
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-02-07 17:36:23 +0530

    temp commit

--- test/dst.test.ts
+++ test/dst.test.ts
@@ -1,89 +1,89 @@
 import { describe, test, expect } from "@jest/globals";
-import { Resonate, Context } from "../lib/resonate";
-import { ResonateTestCrash } from "../lib/core/error";
+// import { Resonate, Context } from "../lib/resonate";
+// import { ResonateTestCrash } from "../lib/core/error";
 
 // trace tree
-type TraceTree = {
-  id: string;
-  func: string;
-  args: any[];
-  children: TraceTree[];
-};
+// type TraceTree = {
+//   id: string;
+//   func: string;
+//   args: any[];
+//   children: TraceTree[];
+// };
 
-function traces(ctx: Context): TraceTree {
-  return {
-    id: ctx.id,
-    func: "",
-    args: [],
-    children: ctx.children.map(traces),
-  };
-}
+// function traces(ctx: Context): TraceTree {
+//   return {
+//     id: ctx.id,
+//     func: "",
+//     args: [],
+//     children: ctx.children.map(traces),
+//   };
+// }
 
-async function test1(ctx: Context) {
-  await ctx.run(foo);
-  await ctx.run(foo);
-}
+// async function test1(ctx: Context) {
+//   await ctx.run(foo);
+//   await ctx.run(foo);
+// }
 
-async function foo(ctx: Context) {
-  await ctx.run(bar);
-  await ctx.run(bar);
-}
+// async function foo(ctx: Context) {
+//   await ctx.run(bar);
+//   await ctx.run(bar);
+// }
 
-async function bar(ctx: Context) {}
+// async function bar(ctx: Context) {}
 
 // prime number generator
-async function test2(ctx: Context) {
-  await ctx.run(prime, 2);
-  await ctx.run(prime, 3);
-  await ctx.run(prime, 5);
-  await ctx.run(prime, 7);
-}
+// async function test2(ctx: Context) {
+//   await ctx.run(prime, 2);
+//   await ctx.run(prime, 3);
+//   await ctx.run(prime, 5);
+//   await ctx.run(prime, 7);
+// }
 
-async function prime(ctx: Context, n: number) {
-  for (let i = 2; i < n; i++) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
+// async function prime(ctx: Context, n: number) {
+//   for (let i = 2; i < n; i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-function isSubsetTree(context: Context, other: Context): boolean {
-  if (context.id !== other.id) {
-    console.log("context.id: " + context.id);
-    return false;
-  }
+// function isSubsetTree(context: Context, other: Context): boolean {
+//   if (context.id !== other.id) {
+//     console.log("context.id: " + context.id);
+//     return false;
+//   }
 
-  const otherChildren = other.children.map((child) => child.id);
-  const contextChildren = context.children.map((child) => child.id);
+//   const otherChildren = other.children.map((child) => child.id);
+//   const contextChildren = context.children.map((child) => child.id);
 
-  if (otherChildren.length > 0 && otherChildren.every((child) => !contextChildren.includes(child))) {
-    return false;
-  }
+//   if (otherChildren.length > 0 && otherChildren.every((child) => !contextChildren.includes(child))) {
+//     return false;
+//   }
 
-  for (const child of context.children) {
-    const matchingChild = other.children.find((otherChild) => otherChild.id === child.id);
+//   for (const child of context.children) {
+//     const matchingChild = other.children.find((otherChild) => otherChild.id === child.id);
 
-    if (matchingChild && !isSubsetTree(child, matchingChild)) {
-      console.log("matchingChild: " + JSON.stringify(matchingChild));
-      return false;
-    }
-  }
-  return true;
-}
+//     if (matchingChild && !isSubsetTree(child, matchingChild)) {
+//       console.log("matchingChild: " + JSON.stringify(matchingChild));
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-function printTree(tree: TraceTree, indent: string = ""): void {
-  console.log(indent + `id: ${tree.id}, func: ${tree.func}, args: ${JSON.stringify(tree.args)}`);
+// function printTree(tree: TraceTree, indent: string = ""): void {
+//   console.log(indent + `id: ${tree.id}, func: ${tree.func}, args: ${JSON.stringify(tree.args)}`);
 
-  if (tree.children.length > 0) {
-    for (let i = 0; i < tree.children.length - 1; i++) {
-      console.log(indent + "├── ");
-      printTree(tree.children[i], indent + "│   ");
-    }
-    console.log(indent + "└── ");
-    printTree(tree.children[tree.children.length - 1], indent + "    ");
-  }
-}
+//   if (tree.children.length > 0) {
+//     for (let i = 0; i < tree.children.length - 1; i++) {
+//       console.log(indent + "├── ");
+//       printTree(tree.children[i], indent + "│   ");
+//     }
+//     console.log(indent + "└── ");
+//     printTree(tree.children[tree.children.length - 1], indent + "    ");
+//   }
+// }
 
 describe("Simulate failures", () => {
   // write a test which will always pass
