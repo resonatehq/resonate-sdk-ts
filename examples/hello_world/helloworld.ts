6382463d98bb8909b1c44266401e26e511fe66f0
Vipul Vaibhaw <vaibhaw.vipul@gmail.com>
2024-03-01 17:23:44 +0530

    si mplifying the helloworld app

--- examples/hello_world/helloworld.ts
+++ examples/hello_world/helloworld.ts
@@ -1,20 +1,5 @@
-import { Resonate } from "../../dist/resonate";
-
-const helloWorld = () => {
-  console.log('Hello, Resonate World!');
-};
+import { Resonate } from '../../dist/resonate';
 
 const resonate = new Resonate();
-resonate.register("helloWorld", helloWorld);
 
-const runExample = async () => {
-  const { context, promise } = resonate.runWithContext("helloWorld", "id");
-
-  console.log('Running...');
-  await promise;
-  console.log(context.children);
-
-  console.log('Done');
-};
-
-runExample();
+console.log('Hello World!');
