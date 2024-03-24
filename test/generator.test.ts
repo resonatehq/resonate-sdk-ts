7e6da05f0365cdeb167192cb48801752a601da56
David Farr <david@resonatehq.io>
2024-03-24 12:26:31 -0700

    Add deferred tests to generator

--- test/generator.test.ts
+++ test/generator.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect, jest } from "@jest/globals";
 import { Retry } from "../lib/core/retries/retry";
+import * as utils from "../lib/core/utils";
 import { Resonate, Context } from "../lib/generator";
 
 jest.setTimeout(10000);
@@ -33,6 +34,14 @@ describe("Generator functions", () => {
     throw new Error("foo");
   }
 
+  function* deferredSuccess(ctx: Context): Generator<any> {
+    return yield ctx.run("success", undefined, ctx.options({ poll: 0 }));
+  }
+
+  function* deferredFailure(ctx: Context): Generator<any> {
+    return yield ctx.run("failure", undefined, ctx.options({ poll: 0 }));
+  }
+
   function* generatorSuccess() {
     return "foo";
   }
@@ -50,19 +59,30 @@ describe("Generator functions", () => {
     resonate.register("run", run);
     resonate.register("call", call);
     resonate.register("callFuture", callFuture);
+    resonate.register("success", generatorSuccess);
+
+    // pre-resolve deferred
+    const deferred = await resonate.promises.create("success", 1000, {
+      idempotencyKey: utils.hash("success"),
+    });
+    deferred.resolve("foo");
 
     const results: string[] = [
       ordinarySuccess(),
       await ordinarySuccessAsync(),
       await resonate.run("run", "run.a", ordinarySuccess),
       await resonate.run("run", "run.b", ordinarySuccessAsync),
-      await resonate.run("run", "run.c", generatorSuccess),
-      await resonate.run("call", "run.d", ordinarySuccess),
-      await resonate.run("call", "run.e", ordinarySuccessAsync),
-      await resonate.run("call", "run.f", generatorSuccess),
-      await resonate.run("callFuture", "run.g", ordinarySuccess),
-      await resonate.run("callFuture", "run.h", ordinarySuccessAsync),
-      await resonate.run("callFuture", "run.i", generatorSuccess),
+      await resonate.run("run", "run.c", deferredSuccess),
+      await resonate.run("run", "run.d", generatorSuccess),
+      await resonate.run("call", "run.e", ordinarySuccess),
+      await resonate.run("call", "run.f", ordinarySuccessAsync),
+      await resonate.run("call", "run.g", deferredSuccess),
+      await resonate.run("call", "run.h", generatorSuccess),
+      await resonate.run("callFuture", "run.i", ordinarySuccess),
+      await resonate.run("callFuture", "run.j", ordinarySuccessAsync),
+      await resonate.run("callFuture", "run.k", deferredSuccess),
+      await resonate.run("callFuture", "run.l", generatorSuccess),
+      await resonate.run("success", "run.m"),
     ];
 
     expect(results.every((r) => r === "foo")).toBe(true);
@@ -77,18 +97,29 @@ describe("Generator functions", () => {
     resonate.register("run", run);
     resonate.register("call", call);
     resonate.register("callFuture", callFuture);
+    resonate.register("failure", generatorFailure);
+
+    // pre-reject deferred
+    const deferred = await resonate.promises.create("failure", 1000, {
+      idempotencyKey: utils.hash("failure"),
+    });
+    deferred.reject(new Error("foo"));
 
     const functions = [
       () => ordinaryFailureAsync(),
       () => resonate.run("run", "run.a", ordinaryFailure),
       () => resonate.run("run", "run.b", ordinaryFailureAsync),
-      () => resonate.run("run", "run.c", generatorFailure),
-      () => resonate.run("call", "run.d", ordinaryFailure),
-      () => resonate.run("call", "run.e", ordinaryFailureAsync),
-      () => resonate.run("call", "run.f", generatorFailure),
-      () => resonate.run("callFuture", "run.g", ordinaryFailure),
-      () => resonate.run("callFuture", "run.h", ordinaryFailureAsync),
-      () => resonate.run("callFuture", "run.i", generatorFailure),
+      () => resonate.run("run", "run.c", deferredFailure),
+      () => resonate.run("run", "run.d", generatorFailure),
+      () => resonate.run("call", "run.e", ordinaryFailure),
+      () => resonate.run("call", "run.f", ordinaryFailureAsync),
+      () => resonate.run("call", "run.g", deferredFailure),
+      () => resonate.run("call", "run.h", generatorFailure),
+      () => resonate.run("callFuture", "run.i", ordinaryFailure),
+      () => resonate.run("callFuture", "run.j", ordinaryFailureAsync),
+      () => resonate.run("callFuture", "run.k", deferredFailure),
+      () => resonate.run("callFuture", "run.l", generatorFailure),
+      () => resonate.run("failure", "run.m"),
     ];
 
     for (const f of functions) {
