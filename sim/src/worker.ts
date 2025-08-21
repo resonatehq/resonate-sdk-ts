e46c867a1936557375c458a697417c29689a5b96
Tomperez98 <tomasperezalvarez@gmail.com>
2025-08-21 09:00:27 -0500

    Fix DST wiring

--- sim/src/worker.ts
+++ sim/src/worker.ts
@@ -10,7 +10,7 @@ interface DeliveryOptions {
 }
 
 class SimulatedNetwork implements Network {
-  private correlationId = 0;
+  private correlationId = 1;
   private currentTime = 0;
 
   private prng: Random;
