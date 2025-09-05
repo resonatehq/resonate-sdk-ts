061fa903f913cae271ecb630f6727eae1269cf6b
David Farr <david@resonatehq.io>
2025-09-05 13:26:38 -0700

    Move run, rpc, get to resonate

--- src/resonate-inner.ts
+++ src/resonate-inner.ts
@@ -2,15 +2,7 @@ import type { Clock } from "./clock";
 import { Computation, type Status } from "./computation";
 import type { Handler } from "./handler";
 import type { Heartbeat } from "./heartbeat";
-import type {
-  CreatePromiseAndTaskReq,
-  CreatePromiseReq,
-  DurablePromiseRecord,
-  Message,
-  Network,
-  ReadPromiseReq,
-  TaskRecord,
-} from "./network/network";
+import type { DurablePromiseRecord, Message, Network, TaskRecord } from "./network/network";
 import type { Registry } from "./registry";
 import type { Callback } from "./types";
 import * as util from "./util";
@@ -44,10 +36,7 @@ export class ResonateInner {
   private registry: Registry;
   private heartbeat: Heartbeat;
   private dependencies: Map<string, any>;
-
   private computations: Map<string, Computation> = new Map();
-  private notifications: Map<string, DurablePromiseRecord> = new Map();
-  private subscriptions: Map<string, Array<(promise: DurablePromiseRecord) => boolean>> = new Map();
 
   constructor({
     unicast,
@@ -83,65 +72,9 @@ export class ResonateInner {
     this.heartbeat = heartbeat;
     this.dependencies = dependencies;
 
-    // subscribe to network messages
-    this.network.subscribe(this.onMessage.bind(this));
-  }
-
-  public run(req: CreatePromiseAndTaskReq): Promise<PromiseHandler> {
-    return new Promise((resolve, reject) => {
-      this.handler.createPromiseAndTask(
-        req,
-        (err, res) => {
-          if (err) {
-            reject(err);
-          } else {
-            // notify
-            this.notify(res!.promise);
-
-            // if we have the task, process it
-            if (res!.task) {
-              this.process({ kind: "claimed", task: res!.task, rootPromise: res!.promise }, () => {});
-            }
-
-            // resolve
-            resolve(this.promiseHandler(res!.promise.id, res!.promise.timeout));
-          }
-        },
-        true,
-      );
-    });
-  }
-
-  public rpc(req: CreatePromiseReq): Promise<PromiseHandler> {
-    return new Promise((resolve, reject) => {
-      this.handler.createPromise(
-        req,
-        (err, res) => {
-          if (err) {
-            reject(err);
-          } else {
-            // notify
-            this.notify(res!);
-            resolve(this.promiseHandler(res!.id, res!.timeout));
-          }
-        },
-        true,
-      );
-    });
-  }
-
-  public get(req: ReadPromiseReq): Promise<PromiseHandler> {
-    return new Promise((resolve, reject) => {
-      this.handler.readPromise(req, (err, res) => {
-        if (err) {
-          reject(err);
-        } else {
-          // huh?
-          this.notify(res!);
-          resolve(this.promiseHandler(res!.id, res!.timeout));
-        }
-      });
-    });
+    // subscribe to invoke and resume
+    this.network.subscribe("invoke", this.onMessage.bind(this));
+    this.network.subscribe("resume", this.onMessage.bind(this));
   }
 
   public process(task: Task, done: Callback<Status>) {
@@ -166,78 +99,11 @@ export class ResonateInner {
     computation.process(task, done);
   }
 
-  private promiseHandler(id: string, timeout: number): PromiseHandler {
-    return {
-      addEventListener: (event: "created" | "completed", callback: (p: DurablePromiseRecord) => void) => {
-        const subscriptions = this.subscriptions.get(id) || [];
-
-        subscriptions.push((p: DurablePromiseRecord): boolean => {
-          if (event === "created" || (event === "completed" && p.state !== "pending")) {
-            callback(p);
-            return true;
-          }
-
-          return false;
-        });
-
-        this.subscriptions.set(id, subscriptions);
-
-        // immediately notify if we already have a promise
-        const promise = this.notifications.get(id);
-        if (promise) {
-          this.notify(promise);
-        }
-      },
-      subscribe: () =>
-        new Promise<void>((resolve, reject) => {
-          this.handler.createSubscription(
-            {
-              kind: "createSubscription",
-              id: id,
-              timeout: timeout + 1 * util.MIN, // add a buffer
-              recv: this.unicast,
-            },
-            (err, res) => {
-              if (err) {
-                reject(err);
-              } else {
-                this.notify(res!);
-                resolve();
-              }
-            },
-            true,
-          );
-        }),
-    };
-  }
-
   private onMessage(msg: Message): void {
-    switch (msg.type) {
-      case "invoke":
-      case "resume":
-        this.process({ kind: "unclaimed", task: msg.task }, () => {});
-        break;
+    util.assert(msg.type === "invoke" || msg.type === "resume");
 
-      case "notify":
-        // TODO(avillega): assert that the promise is completed
-        this.notify(msg.promise);
-        break;
+    if (msg.type === "invoke" || msg.type === "resume") {
+      this.process({ kind: "unclaimed", task: msg.task }, () => {});
     }
-  }
-
-  private notify(promise: DurablePromiseRecord) {
-    // store the notification
-    this.notifications.set(promise.id, promise);
-
-    // notify subscribers
-    const subscriptions = this.subscriptions.get(promise.id) ?? [];
-    for (const [i, f] of subscriptions.entries()) {
-      if (f(promise)) {
-        subscriptions.splice(i, 1);
-      }
-    }
-
-    // remove any subscriber that was notified
-    this.subscriptions.set(promise.id, subscriptions);
   }
 }
