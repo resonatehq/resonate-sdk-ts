2f854bc41a32fee53cc71259945a8681afb65504
Tomperez98 <tomasperezalvarez@gmail.com>
2025-10-17 12:53:05 -0500

    remove app

--- app.ts
+++
@@ -1,9 +0,0 @@
-import {Resonate} from "./src/resonate"
-import {Context} from "./src/context"
-const resonate = Resonate.remote();
-
-resonate.register("foo", () => "foo");
-
-const v = await resonate.run("f1", "foo");
-console.log(v);
-resonate.stop()
