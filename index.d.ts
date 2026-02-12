25588b135ad44d795529b4c203cf1df7b9b40bb7
Andres V <andres.villegas@resonatehq.io>
2026-02-12 15:08:34 -0800

    Commit types so they work with deno pragmas

---
+++ index.d.ts
@@ -0,0 +1,16 @@
+export { WallClock } from "./clock.js";
+export { Context } from "./context.js";
+export { Core, type Task } from "./core.js";
+export { JsonEncoder } from "./encoder.js";
+export { type Encryptor, NoopEncryptor, } from "./encryptor.js";
+export * as essential from "./essential.js";
+export { Handler } from "./handler.js";
+export { AsyncHeartbeat, NoopHeartbeat } from "./heartbeat.js";
+export { HttpNetwork } from "./network/http.js";
+export { Msg } from "./network/types.js";
+export { OptionsBuilder } from "./options.js";
+export { Registry } from "./registry.js";
+export { Resonate, ResonateFunc, ResonateHandle } from "./resonate.js";
+export { NoopTracer } from "./tracer.js";
+export { Func } from "./types.js";
+//# sourceMappingURL=index.d.ts.map
