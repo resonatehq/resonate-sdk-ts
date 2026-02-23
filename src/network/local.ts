d77308aad65f6f94f83f0f830b37339f41d3b36a
Tomás Pérez Álvarez <72174660+Tomperez98@users.noreply.github.com>
2026-02-23 17:07:17 -0500

    Supabase tests (#442)
    * only start network
    
    * uncomment tests
    
    * all tests passing
    
    * review changes

--- src/network/local.ts
+++ src/network/local.ts
@@ -1313,6 +1313,7 @@ export class LocalNetwork implements Network, MessageSource {
   readonly unicast: string;
   readonly anycast: string;
 
+  private started: boolean;
   private server: Server;
   private subscriptions: {
     execute: Array<(msg: Message) => void>;
@@ -1327,6 +1328,7 @@ export class LocalNetwork implements Network, MessageSource {
     pid?: string;
     group?: string;
   } = {}) {
+    this.started = false;
     this.server = new Server();
     this.pid = pid;
     this.group = group;
@@ -1337,6 +1339,7 @@ export class LocalNetwork implements Network, MessageSource {
   // -- Network ---------------------------------------------------------------
 
   start(): void {
+    if (this.started) return;
     this.tickInterval = setInterval(() => {
       const now = Date.now();
       const result = this.server.apply(now, {
@@ -1346,12 +1349,14 @@ export class LocalNetwork implements Network, MessageSource {
       });
       this.dispatchMessages(result);
     }, 1000);
+    this.started = true;
   }
 
   stop(): void {
     if (this.tickInterval) {
       clearInterval(this.tickInterval);
     }
+    this.started = false;
   }
 
   send<K extends Request["kind"]>(
